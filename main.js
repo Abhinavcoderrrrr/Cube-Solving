8da2e230bb06aed0edb5f7d797a82fb70e1e063d
Abhinav Jha <110269551+Abhinavcoderrrrr@users.noreply.github.com>
2025-03-25 12:42:49 +0530

    All file should be included :
    Give credits respectively in a custom credit page.

---
+++ main.js
@@ -0,0 +1,12 @@
+function startweb(){
+    window.location.href = "Solvingpage.html"
+}
+function aboutScreen(){
+    window.location.href = "about.html"
+}
+function credits(){
+    window.location.href = "credit.html"
+}
+function navMain(){
+    window.location.href = "index.html"
+}
